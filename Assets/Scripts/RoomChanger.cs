c47fea36e0e9958336904aa53a695d7b2e3a38cc
mkacprowicz <44576440+mkacprowicz@users.noreply.github.com>
2019-11-04 19:05:41 +0100

    Rooms UI

---
+++ Assets/Scripts/RoomChanger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomChanger : MonoBehaviour
+{
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Molecule"))
+        {
+            other.gameObject.GetComponent<MoleculeMovement>().changeRoom();
+        }
+    }
+}
